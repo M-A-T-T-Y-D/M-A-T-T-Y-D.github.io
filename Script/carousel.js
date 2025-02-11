598f7c5459f1de71c08f3bb27337b22eed22bcb3
M-A-T-T-Y-D <150284530+M-A-T-T-Y-D@users.noreply.github.com>
2025-02-11 20:11:20 +0000

    Changing design
    Making the page look better and be more responsive (working on making the code usable)

---
+++ Script/carousel.js
@@ -0,0 +1,78 @@
+let index = 0;
+let index2 = 0;
+// Open Lightbox
+function openLightbox(slideNumber) {
+    var screenshot;
+    console.log(slideNumber)
+    if (slideNumber == 1) {
+       screenshot = document.getElementById("Screenshot");
+    } else if (slideNumber == 2) {
+        screenshot = document.getElementById("Screenshot1");
+    }
+    const lightboxImg = document.getElementById("lightbox-img");
+
+  
+    lightboxImg.src = screenshot.src;
+    lightbox.classList.add("active");
+  }
+  
+  // Close Lightbox
+  function closeLightbox() {
+    const lightbox = document.getElementById("lightbox");
+    lightbox.classList.remove("active");
+  }
+  
+  // Close Lightbox when clicking outside the image
+  document.getElementById("lightbox").addEventListener("click", function (e) {
+    if (e.target === this) closeLightbox();
+  });
+
+function moveSlide(count) {
+    index += count;
+    switch (index) {
+    case -1:
+        moveSlide(4);
+        break;
+    case 0:
+        document.getElementById("Screenshot").src = "Assets/File_Select.png"
+        break;
+    case 1:
+        document.getElementById("Screenshot").src = "Assets/Main_Menu.png"
+        break;
+    case 2:
+        document.getElementById("Screenshot").src = "Assets/Names.png"
+        break;
+    case 3:
+        document.getElementById("Screenshot").src = "Assets/View.png"
+        break;
+    
+    case 4:
+        moveSlide(-4);
+        break;
+    }
+}
+
+function moveSlide1(count) {
+    index2 += count;
+    switch (index2) {
+    case -1:
+        moveSlide1(4);
+        break;
+    case 0:
+        document.getElementById("Screenshot1").src = "Assets/githubpages 1.png"
+        break;
+    case 1:
+        document.getElementById("Screenshot1").src = "Assets/githubpages 2.png"
+        break;
+    case 2:
+        document.getElementById("Screenshot1").src = "Assets/github pages 3.png"
+        break;
+    case 3:
+        document.getElementById("Screenshot1").src = "Assets/github pages 4.png"
+        break;
+    
+    case 4:
+        moveSlide1(-4);
+        break;
+    }
+}
